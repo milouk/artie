bbd9a7684c20ed02b13044af962bde9f7bf30dc6
Michael Loukeris <loukerismichalis@gmail.com>
2024-09-29 23:27:23 +0300

    Fix launch script

--- Artie Scraper.sh
+++ Artie Scraper.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
-progdir=$(dirname "$0")
-cd $progdir
+#!/bin/sh
+echo app >/tmp/act_go
+
+ARTIE_DIR="$DC_STO_ROM_MOUNT/MUOS/application/.artie"
+
+cd "$ARTIE_DIR" || exit
+
+echo "app" >/tmp/fg_proc
+
 ./app
