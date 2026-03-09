426e1ee8d6a56782377a57db23c5ad2781954234
Michael Loukeris <loukerismichalis@gmail.com>
2026-03-09 19:34:55 +0200

    Refactor launch scripts for improved structure and environment variable management

--- mux_launch.sh
+++ mux_launch.sh
@@ -1,18 +1,53 @@
-#!/bin/sh
+#!/bin/bash
+# HELP: Artie
+# ICON: artie
+# GRID: Artie
+
+. /opt/muos/script/var/func.sh
+
+# Define global variables
+SCREEN_WIDTH=$(GET_VAR device mux/width)
+SCREEN_HEIGHT=$(GET_VAR device mux/height)
+SCREEN_RESOLUTION="${SCREEN_WIDTH}x${SCREEN_HEIGHT}"
+
+# Kill background music if running
+if pgrep -f "playbgm.sh" >/dev/null; then
+	killall -q "playbgm.sh" "mpg123"
+fi
+
 echo app >/tmp/act_go
 
-ARTIE_DIR="/mnt/mmc/MUOS/application/Artie/.artie"
+# Define paths and commands
+ARTIE_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/Artie/.artie"
+GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2.armhf"
+STATICDIR="$ARTIE_DIR/static/"
+BINDIR="$ARTIE_DIR/bin"
 
+# Export environment variables
+export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
+export XDG_DATA_HOME="$STATICDIR"
+export HOME="$STATICDIR"
+export LD_LIBRARY_PATH="$BINDIR/libs.aarch64:$LD_LIBRARY_PATH"
+
+# Launcher
 cd "$ARTIE_DIR" || exit
+SET_VAR "system" "foreground_process" "app"
 
-echo "app" >/tmp/fg_proc
-
+# Define program and log file
 program="./app"
 log_file="${ARTIE_DIR}/log.txt"
 
+# Clear log file
 >"$log_file"
 
-if ! $program >"$log_file" 2>&1; then
+# Run Application with gptokeyb for controller support
+$GPTOKEYB "app" &
+
+if ! $program "${SCREEN_RESOLUTION}" >"$log_file" 2>&1; then
     echo "Error: Failed to execute $program"
+    kill -9 "$(pidof gptokeyb2.armhf)" 2>/dev/null
     exit 1
 fi
+
+# Cleanup
+kill -9 "$(pidof gptokeyb2.armhf)"
