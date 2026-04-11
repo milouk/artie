e34742d8dc8a01839d715e0f6dca6c2b448a5397
Michael Loukeris <loukerismichalis@gmail.com>
2026-04-11 17:14:58 +0300

    Bump version to 4.0.3

--- src/app.py
+++ src/app.py
@@ -39,7 +39,7 @@ from scraper import (
 from settings import SettingsScreen, SystemSelectionScreen, VirtualKeyboard
 from updater import check_for_update, download_and_apply_update
 
-VERSION = "4.0.2"
+VERSION = "4.0.3"
 
 
 class _ScrapeCancelledError(Exception):
